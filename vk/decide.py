665c6fe68c00c3be4b87b31f30b4d25287ff4ac7
Poloz Alexey <polozhev@mail.ru>
2017-07-26 18:40:39 +0300

    All worked vk

---
+++ vk/decide.py
@@ -0,0 +1,13 @@
+from func2 import *
+
+def decide(cont):
+	word=read(name=mas[ii][0]+'-base')[0]
+	with open('data/'+mas[ii][0]+'-weights.csv', 'r') as f:
+		w=np.loadtxt(f, delimiter=',')
+
+	twit=text(cont)
+	su=w[0]
+	for i in range(len(word)):
+		if word[i] in twit:
+			su+=w[i+1]
+	return su
--- vk/func.py
+++ vk/func.py
@@ -4,4 +4,12 @@ vk=vk_api.VkApi(token='44177b16755f6d96acf4fae0a8dd0700472f1e473943769aea355c3b1
 vk.auth()
 
 send=lambda user, cont: vk.method('messages.send', {'user_id':user, 'message':cont})
-read=lambda user: [i['body'] if not i['read_state'] else '' for i in vk.method('messages.get', {'user_id':user})['items']][::-1]
+
+def read(user):
+	cont=[]
+	for i in vk.method('messages.get', {'user_id':user})['items']:
+		if not i['read_state']:
+			cont.append(i['body'])
+	return cont[::-1]
+
+dial=lambda: [i['message']['user_id'] for i in vk.method('messages.getDialogs')['items']]
