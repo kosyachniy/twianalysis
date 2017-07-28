f4a64d018d92ef6479741701672f3f1cfa448dfa
Poloz Alexey <polozhev@mail.ru>
2017-07-29 00:19:53 +0300

    Control time to control sum: close-open / open-close

--- file.py
+++ file.py
@@ -3,9 +3,10 @@ import codecs
 
 delete('twits')
 
-def stock(ye, mo, da):
-	data=ye+'-'+mo+'-'+da
-	tim=int(ye+mo+da)
+def stock(da, mo, ye):
+	data=str(ye)+'-'+str(mo)+'-'+str(da)
+	tim=(ye*100+mo)*100+da
+
 	if tim>=20140609:
 		border='tqbr'
 	elif tim>=20131226:
@@ -14,25 +15,73 @@ def stock(ye, mo, da):
 		border='tqne'
 	elif tim>=20060123:
 		border='eqne'
+
 	j=json.loads(get('http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/'+border+'/securities.json?date='+data))['history']
 	col=j['columns']
-	l=0
 	for i in j['data']:
 		if i[col.index('SECID')]==mas[ii][2]:
-			l=round(i[col.index('CLOSE')]-i[col.index('OPEN')],2)
-	return l if l else 0
+			return i[col.index('CLOSE')], i[col.index('OPEN')]
+	return 0, 0
 
-if __name__=='__main__':
-	k=0
-	with codecs.open('data/'+mas[ii][0]+'-news.txt', 'r', 'utf8') as file:
-		for i in file:
-			s=json.loads(i)
-			mood=stock(s['date'][6:10], s['date'][3:5], s['date'][0:2])
-			try:
-				write([mood]+text(s['name']), name='twits', typ='a')
-			except:
-				print('Error in string:'+s)
+m=[]
+with codecs.open('data/'+mas[ii][0]+'-news.txt', 'r', 'utf8') as file:
+	for i in file:
+		date=json.loads(i)['date']
+		day=int(date[0:2])
+		month=int(date[3:5])
+		year=int(date[6:10])
+		hour=int(date[12:14])
+		minute=int(date[15:17])
+
+		if (year*100+month)*100+day<20060123: break
+		m.append([day, month, year, hour, minute, json.loads(i)['name']])
+
+print(len(m))
+
+def pred(i):
+	for j in m[:i][::-1]:
+		if j[0]!=m[i][0]:
+			return j[0], j[1], j[2]
+	return ()
+
+def sled(i):
+	for j in m[i:]:
+		if j[0]!=m[i][0]:
+			return j[0], j[1], j[2]
+	return ()
+
+k=0
+n=[]
+for i in range(len(m)):
+	try:
+		if m[i][3]*100+m[i][4]>1830:
+			q=pred(i)
+			if len(q):
+				mood=stock(*q)[1]-stock(m[i][0], m[i][1], m[i][2])[0]
 			else:
-				k+=1
-				print(k, mood)
-			time.sleep(1)
+				continue
+		elif hour*100+minute<1000:
+			q=sled(i)
+			if len(q):
+				mood=stock(m[i][0], m[i][1], m[i][2])[1]-stock(*q)[0]
+			else:
+				continue
+		else:
+			cl, op=stock(m[i][0], m[i][1], m[i][2])
+			mood=cl-op
+	except:
+		print('Error in string:', m[i])
+	else:
+		k+=1
+		print(k, mood)
+	time.sleep(1)
+
+	if len(n) and n[len(n)-1][0]==m[i][0]:
+		n[len(n)-1].extend(text(m[i][5]))
+	else:
+		n.append([m[i][0], mood]+text(m[i][5]))
+
+print(len(n))
+
+for i in n:
+	write(i[1:], name='twits', typ='a')
